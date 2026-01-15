483cbc1811d2d0d5ca4b6f630d314386ff8e0317
infiniterafa <ji.rafa.el05@gmail.com>
2026-01-15 09:12:39 -0600

    Agregar archivos de proyecto.

---
+++ TestRayLib/TestRayLib.cpp
@@ -0,0 +1,153 @@
+// TestRayLib.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina a
+/*******************************************************************************************
+*
+*   raylib [core] example - basic window
+*
+*   Example complexity rating: [★☆☆☆] 1/4
+*
+*   Welcome to raylib!
+*
+*   To test examples, just press F6 and execute 'raylib_compile_execute' script
+*   Note that compiled executable is placed in the same folder as .c file
+*
+*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
+*   Web version of the program is generated in the same folder as .c file
+*
+*   You can find all basic examples on C:\raylib\raylib\examples folder or
+*   raylib official webpage: www.raylib.com
+*
+*   Enjoy using raylib. :)
+*
+*   Example originally created with raylib 1.0, last time updated with raylib 1.0
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2013-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+#define NUM_FRAMES_PER_LINE     5
+#define NUM_LINES               5
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - sprite explosion");
+
+    InitAudioDevice();
+
+    // Load explosion sound
+    Sound fxBoom = LoadSound("assets/boom.wav");
+
+    // Load explosion texture
+    Texture2D explosion = LoadTexture("assets/explosion.png");
+
+    // Init variables for animation
+    float frameWidth = (float)(explosion.width / NUM_FRAMES_PER_LINE);   // Sprite one frame rectangle width
+    float frameHeight = (float)(explosion.height / NUM_LINES);           // Sprite one frame rectangle height
+    int currentFrame = 0;
+    int currentLine = 0;
+
+    Rectangle frameRec = { 0, 0, frameWidth, frameHeight };
+    Vector2 position = { 0.0f, 0.0f };
+
+    bool active = false;
+    int framesCounter = 0;
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //---------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+
+        // Check for mouse button pressed and activate explosion (if not active)
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !active)
+        {
+            position = GetMousePosition();
+            active = true;
+
+            position.x -= frameWidth / 2.0f;
+            position.y -= frameHeight / 2.0f;
+
+            PlaySound(fxBoom);
+        }
+
+        // Compute explosion animation frames
+        if (active)
+        {
+            framesCounter++;
+
+            if (framesCounter > 2)
+            {
+                currentFrame++;
+
+                if (currentFrame >= NUM_FRAMES_PER_LINE)
+                {
+                    currentFrame = 0;
+                    currentLine++;
+
+                    if (currentLine >= NUM_LINES)
+                    {
+                        currentLine = 0;
+                        active = false;
+                    }
+                }
+
+                framesCounter = 0;
+            }
+        }
+
+        frameRec.x = frameWidth * currentFrame;
+        frameRec.y = frameHeight * currentLine;
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        // Draw explosion required frame rectangle
+        if (active) DrawTextureRec(explosion, frameRec, position, WHITE);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(explosion);   // Unload texture
+    UnloadSound(fxBoom);        // Unload sound
+
+    CloseAudioDevice();
+
+    CloseWindow();              // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+
+// clase GameEngine 
+// init()
+// update 
+// draw()
+// run()
+
+//Engine engine; 
+//engine.initialize(); 
+//engine.run(); 
+//engine.shutdown(); 
